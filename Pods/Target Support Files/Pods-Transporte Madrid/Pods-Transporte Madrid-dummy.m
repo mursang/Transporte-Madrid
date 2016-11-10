2ed6e28a166fb234618d11d8391db691aa8993ae
mursang <angel8492@hotmail.com>
2016-11-10 11:39:54 +0100

    no message

--- Pods/Target Support Files/Pods-Transporte Madrid/Pods-Transporte Madrid-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Transporte_Madrid : NSObject
-@end
-@implementation PodsDummy_Pods_Transporte_Madrid
-@end
--- Pods/Target Support Files/Pods-Transporte Madrid/Pods-Transporte Madrid-umbrella.h
+++
@@ -1,8 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-
-FOUNDATION_EXPORT double Pods_Transporte_MadridVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_Transporte_MadridVersionString[];
-
